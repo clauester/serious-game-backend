ead0973eb70bc75609889558a3ea4dfc5f26a5a9
Luis Armando <luicagua65@gmail.com>
2026-02-01 00:39:01 -0500

    whitespaces deleted on game related files

--- src/Service/GameService.php
+++ src/Service/GameService.php
@@ -1,30 +1,30 @@
-
 <?php
 
 require_once __DIR__ . '/../Repository/GameRepository.php';
 
-class GameService {
-
+class GameService
+{
     private $repo;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->repo = new GameRepository();
     }
 
-    public function getAllGames(string $action) {
+    public function getAllGames(string $action)
+    {
         return $this->repo->getAllGames($action);
     }
-    
-    public function createGame( 
-        string $action, 
-        ?string $user_id, 
-        ?string $group_id, 
-        ?string $status, 
+
+    public function createGame(
+        string $action,
+        ?string $user_id,
+        ?string $group_id,
+        ?string $status,
         ?int $grade,
         ?string $gameId
-        ) {
+    ) {
         // Lógica para crear un juego
         return $this->repo->createGame($action, $user_id, $group_id, $status, $grade, $gameId);
     }
-
 }
